4a0001b2d83fab35cfcf2fd858e58bd965508b71
Stavya Joshi <joshistavya@gmail.com>
2026-03-11 21:26:54 +0530

    fixed deployment issues

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-
+"""
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
@@ -10,5 +10,24 @@ cred = credentials.Certificate(FIREBASE_CREDENTIALS)
 firebase_admin.initialize_app(cred)
 
 def verify_user(token:str):
+    decoded = auth.verify_id_token(token)
+    return decoded"""
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, auth
+
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+
+# convert string -> dict
+cred_dict = json.loads(firebase_json)
+
+cred = credentials.Certificate(cred_dict)
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+def verify_user(token: str):
     decoded = auth.verify_id_token(token)
     return decoded
